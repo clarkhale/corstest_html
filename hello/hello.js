bac4a3b07aa72820ba6afdf8ab712fb7f2d91bcc
Clark Hale <chale@redhat.com>
2020-08-23 22:07:50 -0400

    changed back to https

--- hello/hello.js
+++ hello/hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://corstest.apps.cfh-test.sandbox1479.opentlc.com/greeting"
+        url: "https://corstest.apps.cfh-test.sandbox1479.opentlc.com/greeting"
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
