c900b0a6f3f48bf27f66465dc923a65001618e8f
Clark Hale <chale@redhat.com>
2020-08-25 08:32:43 -0400

    changed to http

--- hello/hello.js
+++ hello/hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://wildcard.corstest.apps.cfh-test.sandbox1479.opentlc.com/greeting"
+        url: "http://wildcard.corstest.apps.cfh-test.sandbox1479.opentlc.com/greeting"
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
